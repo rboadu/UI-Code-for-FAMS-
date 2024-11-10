26973205ccb16c114eeb5cb5f8c755e53c5ce310
NaidelynHernandez <Naidelynhernandez12@gmail.com>
2024-11-10 17:06:09 -0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
+import React from 'react';
+import { useRoutes, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./components/Login";
+import Home from "./components/Home";
 import Prof from "./components/Prof"; 
 import './App.css'
 
-
 function App() {
- 
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/Login',
+      element: <Login />,
+    },
+    {
+      path: '/update',
+      element: <Prof />,
+    },
+  ]);
 
   return (
     <>
-      <div>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>  || 
+          <Link to="/Login"> Login</Link> || 
+          <Link to="/Prof"> Professor Chatbot</Link> 
+        </nav>
+
+          {routes}
       </div>
-
-      <div> <Login /></div>
-    <div> <Prof/></div>
-
-    
-    
-      </>
+    </>
   )
 }
 
